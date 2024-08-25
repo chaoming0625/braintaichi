4796c6a905209d3631f6ff41618c97cbfe6b9e98
He Sichao <1310722434@qq.com>
2024-08-25 22:37:01 +0800

    another way to cibuildwheel with specific python version

--- copy_pyproject_mac_x86_64.sh
+++ copy_pyproject_mac_x86_64.sh
@@ -4,16 +4,14 @@
 python_version=$(python -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
 
 # Set the CIBW_BUILD environment variable based on the Python version
-if [[ $python_version == "3.8" ]]; then
-    export CIBW_BUILD="cp38-macosx_x86_64"
-elif [[ $python_version == "3.9" ]]; then
-    export CIBW_BUILD="cp39-macosx_x86_64*"
+if [[ $python_version == "3.9" ]]; then
+    cp ci/mac/x86_64/pyproject3.9.toml pyproject.toml
 elif [[ $python_version == "3.10" ]]; then
-    export CIBW_BUILD="cp310-macosx_x86_64*"
+    cp ci/mac/x86_64/pyproject3.10.toml pyproject.toml
 elif [[ $python_version == "3.11" ]]; then
-    export CIBW_BUILD="cp311-macosx_x86_64"
+    cp ci/mac/x86_64/pyproject3.11.toml pyproject.toml
 elif [[ $python_version == "3.12" ]]; then
-    export CIBW_BUILD="cp312-macosx_x86_64"
+    cp ci/mac/x86_64/pyproject3.12.toml pyproject.toml
 else
     echo "Unsupported Python version: $python_version"
     exit 1
