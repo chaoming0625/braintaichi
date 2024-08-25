3da53b0c87f81db466b001dad11b64ea537f6580
He Sichao <1310722434@qq.com>
2024-08-25 23:22:12 +0800

    Revert "Update"
    This reverts commit 7347b5603b15c38e356d520f1a7f45de8488d2e3.

--- copy_so_macOS.sh
+++ copy_so_macOS.sh
@@ -5,18 +5,8 @@ chmod +x ./copy_so_macOS.py
 python copy_so_macOS.py
 
 # Ensure the set_env.sh script is actually created and is not empty
-# Get the absolute path of the project/braintaichi directory
-project_path="$(dirname "$(realpath "$0")")/project/braintaichi"
+source project/set_env.sh
+cp "$taichi_runtime_lib_dir"/libtaichi_c_api.dylib project/braintaichi/
 
-# Set the absolute path as an environment variable
-export PROJECT_PATH="$project_path"
-
-# Source the set_env.sh script
-source "$project_path/set_env.sh"
-
-# Copy the libtaichi_c_api.dylib file to the project/braintaichi directory
-cp "$taichi_runtime_lib_dir"/libtaichi_c_api.dylib "$project_path/"
-
-# List the contents of the project/braintaichi directory
-ls "$project_path"
+ls project/braintaichi/
 
