d5d8f8b49fd3db5a0da36733bf3b424a5412bf7d
He Sichao <1310722434@qq.com>
2024-09-12 18:43:47 +0800

    Update prepare_build_wheel_on_cuda.sh

--- ci/linux/gpu/prepare_build_wheel_on_cuda.sh
+++ ci/linux/gpu/prepare_build_wheel_on_cuda.sh
@@ -74,7 +74,7 @@ mv setup_cuda.py setup.py
 
 # yum install --disableplugin=fastmirror -y python3-devel.x86_64
 
-get https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
